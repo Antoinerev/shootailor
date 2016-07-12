402bf731694d697d6e53ae7db49a27856133cc82
Antoine Reveau <antoinerev@gmail.com>
2016-07-12 18:52:28 +0200

    new booking form added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,6 +6,8 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @photog = User.find(params[:user_id])
+    @booking = Booking.new()
   end
 
   def create
@@ -19,4 +21,14 @@ class BookingsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  # def user_params
+  #   params.require(:user).permit(:first_name, :last_name, :email, :address, :avatar, :portfolio, :tags, :bio, :price, :photog)
+  # end
+
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 end
