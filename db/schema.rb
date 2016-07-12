63ec03072fb8fe4c54e7a3c2c25d1946345b7150
paulgaumer <mitsudomoe8@gmail.com>
2016-07-12 11:55:02 +0200

    Deleted :email column in Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711215119) do
+ActiveRecord::Schema.define(version: 20160712095240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20160711215119) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
     t.string   "address"
     t.string   "avatar"
     t.string   "portfolio"
