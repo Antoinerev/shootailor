109dbbb9509ee8a83508ab068e7c6c7f4d5471f7
paulgaumer <mitsudomoe8@gmail.com>
2016-07-14 20:33:18 +0200

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20160714151646) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
     t.string   "address"
     t.string   "avatar"
     t.string   "portfolio"
@@ -58,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160714151646) do
     t.boolean  "photog"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "email"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
